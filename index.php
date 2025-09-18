2a4013d5f1b51f2094a6c0bf65e449f0de7fc30f
Grace Joseph <gracecjoseph5@gmail.com>
2025-09-18 14:52:37 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,420 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grace Joseph</title>
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" /> -->
+    <script src="https://kit.fontawesome.com/4fe59c631b.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css" />
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+</head>
+
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+
+    <!-- Header -->
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#"><span class="logo-m black">JCG</span><span class="logo-m black">.com</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn"><strong>Home</strong></a></li>
+                <li><a href="#about" class="menu-btn"><strong>About</strong></a></li>
+                <li><a href="#services" class="menu-btn"><strong>Services</strong></a></li>
+                <li><a href="#skills" class="menu-btn"><strong>Skills</strong></a></li>
+                <li><a href="#projects" class="menu-btn"><strong>Projects</strong></a></li>
+                <li><a href="#contact" class="menu-btn"><strong>Contact</strong></a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Home -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">Hello, This is</div>
+                <div class="text-2">Grace Chioma Joseph</div>
+                <div class="text-3">And I'm a <span class="typing"></span></div>
+                <a href="mailto:gracecjoseph5@gmail.com">Hire me</a>
+            </div>
+        </div>
+    </section>
+
+    <!--  About  -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About Me</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="img.jpg" alt="Profile Image 626x626">
+                </div>
+                <div class="column right">
+                    <div class="text">I'm Grace and I'm a <span class="typing-2"></span></div>
+                    <p>Seeking an opportunity with an esteemed organization where I can utilize my skills & enhance learning in the field of work. Capable of mastering new technologies.</p>
+                    <br>
+                    <div class="text">Why Work With Me</div>
+                    <p>I'm a great communicator & love to invest the necessary time to understand the customer's problem very well.</p>
+
+                    <a href="cv/Joseph-Grace-main.pdf" target="blank">Download CV</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Services -->
+    <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">My services</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-laptop-code"></i>
+                        <div class="text">Web Development</div>
+                        <p>A developer who builds efficient, user-friendly, and innovative digital solutions..</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-user-secret"></i>
+                        <div class="text">User Secret</div>
+                        <p>I ensure that, there is no sensitive data included in the source code and are stored outside of the project folder. And all the data which is stored by User Secrets is not encrypted.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-code"></i>
+                        <div class="text">Web Design</div>
+                        <p>I encompass both the user interface(UI) & user experience(UX). The overall style of the app, including things like the colour scheme, font selection, and the types of buttons and widgets which will be used.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Skills -->
+    <section class="skills" id="skills">
+        <div class="max-width">
+            <h2 class="title">My Skills</h2>
+            <div class="skills-content">
+                <div class="column left">
+
+                    <div class="text">INTERNSHIP</div>
+                    <p>
+                        <li class="intern">Agrandi Technologies</li>
+                        <!-- <li class="intern">#Company Name "Where You work"</li>
+            <li class="intern">#Company Name "Where You work"</li> -->
+                    </p>
+
+                    <br>
+                    <div class="text">PROJECTS</div>
+
+                    <p>
+                        <!-- <li class="project">Portfolio (HTML, CSS, js, Bootstraps)</li> -->
+                        <li class="project"><a href="https://azerscinema.org/">Azers Cinema (Php, HTML, CSS, Javascript, Bootstrap)</a></li>
+                        <li class="project"><a href="https://mymakeupproject.netlify.app/">Makeup API Project - (Next.js, Context API, Tailwind css)</a></li>
+                        <li class="project"><a href="https://myspacetouch.netlify.app/">Space Touch Travels and Tours (React.js, Tailwind css)</a></li>
+                        <li class="project"><a href="https://ecurrencytask.netlify.app/">E - Currency Task (Next.js, Tailwind Css)</a></li>
+                        <!-- <li class="project">D O A</li> -->
+                    </p>
+
+                    <br>
+                    <div class="text">DEVELOPMENT SKILLS</div>
+
+                    <p>
+                        I'm familiar & work on a daily basis with PHP, HTML, CSS, JavaScript, Bootstrap, React js, and Next.js
+                    </p>
+                    <a href="gracecjoseph5@gmail.com" target="blank">Hire Me...</a>
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>Php</span>
+                            <span>70%</span>
+                        </div>
+                        <div class="line python"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>HTML</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>CSS</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>JavaScript</span>
+                            <span>50%</span>
+                        </div>
+                        <div class="line js"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>Bootstraps</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line bootstraps"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>Next.js</span>
+                            <span>50%</span>
+                        </div>
+                        <div class="line django"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>React.js</span>
+                            <span>50%</span>
+                        </div>
+                        <div class="line django"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Projects -->
+    <section class="projects" id="projects">
+        <div class="max-width">
+            <h2 class="title">My Projects</h2>
+            <div class="carousel owl-carousel">
+                <div class="card">
+                    <div class="box">
+                        <img src="azers.jpg" alt="">
+                        <div class="text">Azers Cinema</div>
+                        <p>Your premier destination for movie tickets and entertainment</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="makeup.jpg" alt="">
+                        <div class="text">Makeup - Ecommerce</div>
+                        <p>Your one-stop shop for all things makeup.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="spacetouch.jpg" alt="">
+                        <div class="text">spacetouch</div>
+                        <p>Spacetouch Travels strives to provide her customers with the very best service in the travel and tours industry.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="ecurrency.jpg" alt="">
+                        <div class="text">E - Currency</div>
+                        <p>With the aid of our Melatonin Sleepstiq, we can assure you that you can get quality sleep.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Contact Me</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Get in Touch</div>
+                    <p>Feel free to get in touch with me. I am always open to discussing new projects, creative ideas or opportunities to be a part of your visions. Email me with any questions or inquiries. I'll be happy to answer your questions and set up a meeting with you.</p>
+                    <div class="icons">
+
+                        <a href="https://instagram.com/aistscience">
+                            <div class="row">
+                                <i class="fa-solid fa-user-large"></i>
+                                <div class="info">
+                                    <div class="head">Name</div>
+                                    <div class="sub-title">Grace Chioma Jospeh</div>
+                                </div>
+                            </div>
+                        </a>
+
+                        <div class="row">
+                            <i class="fa-solid fa-location-dot"></i>
+                            <div class="info">
+                                <div class="head">&nbsp;Address</div>
+                                <div class="sub-title">&nbsp;Aseese, Ogun State.</div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <i class="fa-solid fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title"><a href="mailto:gracecjoseph5@gmail.com" style="text-decoration: none; color: #333;"> gracecjoseph5@gmail.com</a></div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <i class="bi bi-translate"></i>
+                            <div class="info">
+                                <div class="head">Language Known</div>
+                                <div class="sub-title">English</div>
+                            </div>
+                        </div>
+
+                        <br>
+
+                        <!-- Social -->
+                        <div class="social-menu">
+                            <ul>
+                                <li><a href="https://github.com/GraceJoseph-hub"><i class="fa-brands fa-github"></i></a></li>
+                                <li><a href="https://www.linkedin.com/in/josephcgrace/"><i class="fa-brands fa-linkedin-in"></i></a></li>
+                                <li><a href="https://www.instagram.com/gracejoseph_karis/"><i class="fa-brands fa-instagram"></i></a></li>
+                                <li><a href="https://x.com/ShebuildsTech31"><i class="fa-brands fa-twitter"></i></a></li>
+                                <!-- <li><a href="https://aistechx.com/" title="Get Educate with Us"><i class="fa-solid fa-facebook"></i></a></li> -->
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Reach -->
+                <div class="column right">
+                    <div class="text">Message me</div>
+                    <form action="#">
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" placeholder="Your Name" required>
+                            </div>
+                            <div class="field email">
+                                <input type="email" placeholder="Your Email" required>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" placeholder="Subject" required>
+                        </div>
+                        <div class="field textarea">
+                            <textarea cols="30" rows="10" placeholder="Message.." required></textarea>
+                        </div>
+                        <div class="button-area">
+                            <button type="submit">Send message</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer>
+        <span>
+            <a href="mailto:gracecjoseph5@gmail.com"> Grace Chioma Joseph</a> | <span class="far fa-copyright"></span> All Rights Reserved. Privacy Policy
+        </span>
+    </footer>
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+
+    <script>
+        $(document).ready(function () {
+  $(window).scroll(function () {
+    //  sticky navbar on scroll script  //
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+
+     if (this.scrollY > 20) {
+      $(".logo-m").addClass("white");
+    } else {
+      $(".logo-m").removeClass("black");
+    }
+
+    //  scroll-up button show/hide script  //
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  //  slide-up script  //
+
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    //  removing smooth scroll on slide-up button click  //
+    $("html").css("scrollBehavior", "auto");
+  });
+
+  $(".navbar .menu li a").click(function () {
+    //  Smooth scroll on Menu Items click  //
+
+    $("html").css("scrollBehavior", "smooth");
+  });
+
+  //  Toggle Navbar  //
+
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+
+  //  Typing Text Animation  //
+
+  var typed = new Typed(".typing", {
+    strings: [
+      "Fullstack Developer",
+      "Software Developer",
+      "Php Developer",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: [
+      "Fullstack Developer",
+      "Software Developer",
+      "Php Developer",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
+  //  Owl Carousel  //
+
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      600: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: false
+      }
+    }
+  });
+});
+
+    </script>
+   
+
+</body>
+
+</html>
